0a3805f9097652533f50d67f8e1587395b654523
sreekanth.y <sreekanth.y@bestinet.in>
2020-11-02 16:17:20 +0530

    day 01 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,38 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World Test Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home page'),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: Center(
-          child:
-          Text(
-            'Welcome Bestinet India Pvt Ltd',
-          )
-      ),
-    );
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body:
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children:<Widget>[
+              Container(
+                margin: EdgeInsets.all(12.0),
+                padding: EdgeInsets.all(8.0),
+                decoration:BoxDecoration(
+                    borderRadius:BorderRadius.circular(8),
+                    color:Colors.green
+                ),
+                child: Text("Flutter",style: TextStyle(color:Colors.yellowAccent,fontSize:25),),
+              ),
+
+            ]
+        )
+        ),
+      );
+
   }
 }
